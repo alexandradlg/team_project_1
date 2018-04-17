2fa256754b05b2a2689c296810aac50803f8d31e
Abderrahim <aitbali@hotmail.com>
2018-04-17 21:07:55 +0200

    branche terminée!

--- send_emails/Send_Emails.rb
+++ send_emails/Send_Emails.rb
@@ -14,31 +14,31 @@ puts
 
 def get_emails  #method ed envoi d emails aux mairies 
 
-email = ""  #creation d une variable vide de type chaine de carfactére pour stocker les emails
+email2 = ""  #creation d une variable vide de type chaine de carfactére pour stocker les emails
 ville = ""  #creation d une variable vide de type chaine de carfactére pour stocker les noms des villes
 
-CSV.foreach('../data2.csv') do |row| #pour chaque ligne du fichier csv data2.csv on crée une variable row  
-
+CSV.foreach('../scrapper/data2.csv') do |row| #pour chaque ligne du fichier csv data2.csv on crée une variable row  
+next if $. == 1
     ville = "#{row[0]}" #on recupére la valeur de la ligne traitée en colonne 0 (1 ére colonne) qui corespond à la ville
-    email = "#{row[2]}" #on recupére la valeur de la ligne traitée en colonne 2 (3eme colonne) qui correspond à l' email
+    email2 = "#{row[2]}" #on recupére la valeur de la ligne traitée en colonne 2 (3eme colonne) qui correspond à l' email
 
         #on se connecte avec les identifiants de notre boite mail d envoi à Gmail
         gmail = Gmail.connect(ENV['GMAIL_ID'],ENV['GMAIL_SECRET'])
-
-        
-        email = gmail.compose do #pour chaque ligne du tableau csv donc pour chaque ville , on envoie le smail cidessous.
-            if email == nil
-                return true 
-            else 
-                to "email"           #adresse de destination
-            subject "Apprendre à coder pour l avenir!"#sujet de l email
-            #message
-            body  "Bonjour,
-            nous sommes un groupe d éleves , nous sommes élèves à The Hacking Project, une formation au code gratuite, sans locaux, sans sélection, sans restriction géographique. La pédagogie de ntore école est celle du peer-learning, où nous travaillons par petits groupes sur des projets concrets qui font apprendre le code. Le projet du jour est d'envoyer (avec du codage) des emails aux mairies pour qu'ils nous aident à faire de The Hacking Project un nouveau format d'éducation pour tous.
-            Déjà 300 personnes sont passées par The Hacking Project. Est-ce que la mairie de #{ville} veut changer le monde avec nous ?
-            Charles, co-fondateur de The Hacking Project pourra répondre à toutes vos questions : 06.95.46.60.80."
-            end
-        
+        if email2.nil? || email2.empty?
+        else
+            puts ville
+            email = gmail.compose do #pour chaque ligne du tableau csv donc pour chaque ville , on envoie le smail cidessous.
+            
+                to "#{email2}"           #adresse de destination
+                subject "Apprendre à coder pour l avenir!"#sujet de l email
+                #message
+                body  "Bonjour,
+                nous sommes un groupe d éleves , nous sommes élèves à The Hacking Project, une formation au code gratuite, sans locaux, sans sélection, sans restriction géographique. La pédagogie de ntore école est celle du peer-learning, où nous travaillons par petits groupes sur des projets concrets qui font apprendre le code. Le projet du jour est d'envoyer (avec du codage) des emails aux mairies pour qu'ils nous aident à faire de The Hacking Project un nouveau format d'éducation pour tous.
+                Déjà 300 personnes sont passées par The Hacking Project. Est-ce que la mairie de #{ville} veut changer le monde avec nous ?
+                Charles, co-fondateur de The Hacking Project pourra répondre à toutes vos questions : 06.95.46.60.80."
+            end 
+            
+            
         end
         email.deliver!  #envoi de l email
         
